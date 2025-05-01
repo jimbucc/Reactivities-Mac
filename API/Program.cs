d9bdc60137c44820df8b090bd9e315eea8fdee6f
jimbucc <james.bucciferro@perkinelmer.com>
2025-04-30 21:00:46 -0400

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,12 +9,14 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
